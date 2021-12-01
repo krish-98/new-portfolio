6c6382d04b2826c4ac56437d068571038a0668eb
krish-98 <muralikrishnanm98@gmail.com>
2021-12-01 13:36:19 +0530

    added hero section with svg animation

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -1,13 +1,23 @@
-import React from 'react';
+import React from 'react'
 
-import { Section, SectionText, SectionTitle } from '../../styles/GlobalComponents';
-import Button from '../../styles/GlobalComponents/Button';
-import { LeftSection } from './HeroStyles';
+import { Section, SectionText, SectionTitle } from '../../styles/GlobalComponents'
+import Button from '../../styles/GlobalComponents/Button'
+import { LeftSection } from './HeroStyles'
 
-const Hero = (props) => (
-  <div>
-    Hero
-  </div>
-);
+const Hero = () => (
+  <Section row nopadding>
+    <LeftSection>
+      <SectionTitle main center>
+        Bonjour! I'm <br />
+        Murali krishnan <br />
+        Front-End Developer
+      </SectionTitle>
+      <SectionText>
+      I build responsive Websites & Web Apps using modern-day technologies which include HTML5, CSS3, JavaScript, React.JS, and lot more.
+      </SectionText>
+      <Button onClick={() => window.location = "https://google.com"}>To Know More</Button>
+    </LeftSection>
+  </Section>
+)
 
-export default Hero;
+export default Hero
